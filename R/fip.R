9fb26ebe267be303edc4b3922b4c1d3978ebf769
shunsuke <s_shunske@yahoo.co.jp>
2026-02-21 15:32:20 +0800

    Changes related to a change of the project name from 'RMyUtilities' to 'myutils'.

--- R/fip.R
+++ R/fip.R
@@ -4,24 +4,32 @@
 
 #' Darcy-Weisbach equation
 #' @param fD       Darcy friction factor
-#' @param density  density
-#' @param velocity velocity
-#' @param D        pipe diameter
-#' @return         dP per unit length
+#' @param density  density (kg/m3)
+#' @param velocity velocity (m/s)
+#' @param D        pipe diameter (m)
+#' @return         dP per unit length (Pa/m)
 #' @export
 DarcyWeisbach <- function(fD, density, velocity, D) {
   fD * density * (velocity ^ 2) / (2 * D)
 }
 
+#' Darcy friction factor in laminar flow
+#' @param Re Reynolds number
+#' @return   Darcy friction factor
+#' @export
 fD_laminar <- function(Re) { 64 / Re }
 
 #' Blasius correlation to calculate Darcy friction factor
 #' @param Re Reynolds number
-#' @return Darcy friction factor
+#' @return   Darcy friction factor
 #' @export
 fD_Blasius <- function(Re, C=0.3164) { C / (Re ^ 0.25) }
 
 #' Colebrook correlation to calculate Darcy friction factor
+#' @param roughness roughness (m)
+#' @param D         diameter (m)
+#' @param Re        Reynolds number
+#' @return          Darcy friction factor
 #' @export
 fD_Colebrook <- function(roughness, D, Re, tol=1e-8, itMax=10, warn=TRUE) {
   core_ <- function(roughness, D, Re) {
--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# RMyUtilities
+# myutils
 
 ## Installing 
 
